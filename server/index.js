ac8de2196072fa0a207a30b747db07157fd9dace
RahulBis <rahulsinghbisht125@gmail.com>
2024-06-03 15:26:26 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -28,10 +28,10 @@ const io = new Server(server, {
   },
 });
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 });
 
 app.get("/", (req, res) => {
