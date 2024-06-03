affce40048adc2c18060e325f794cbd44f300f8c
RahulBis <rahulsinghbisht125@gmail.com>
2024-06-03 16:08:14 +0530

    commit

--- server/database/dbConnect/connectDB.js
+++ server/database/dbConnect/connectDB.js
@@ -1,16 +1,39 @@
+// import mongoose from 'mongoose';
+
+// const connectDB = async (DATABASE_URL) => {
+//     try {
+//         const DB_OPATIONS={
+//             dbName:"suportCartApp",
+//         }
+//         await mongoose.connect(DATABASE_URL,DB_OPATIONS);
+//         console.log("Connected to database successfully 🚀");
+        
+//     } catch (error) {
+//         return Promise.reject(error);
+//     }
+// }
+
+// export default connectDB;
+
 import mongoose from 'mongoose';
 
 const connectDB = async (DATABASE_URL) => {
     try {
-        const DB_OPATIONS={
-            dbName:"suportCartApp",
-        }
-        await mongoose.connect(DATABASE_URL,DB_OPATIONS);
+        const DB_OPTIONS = {
+            dbName: "supportCartApp",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            tls: true,
+            tlsAllowInvalidCertificates: true, // Use only for testing, not recommended for production
+            tlsInsecure: true // Use only for testing, not recommended for production
+        };
+
+        await mongoose.connect(DATABASE_URL, DB_OPTIONS);
         console.log("Connected to database successfully 🚀");
-        
     } catch (error) {
+        console.error("Error connecting to database", error);
         return Promise.reject(error);
     }
-}
+};
 
 export default connectDB;
